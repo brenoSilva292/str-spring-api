ef868c1e74c157843c342ab0b5de5f890ed1ce10
Breno Silva <brenolevy292@gmail.com>
2025-08-21 16:04:57 -0400

    Adicionando código-fonte completo do projeto Spring

--- src/main/java/br/com/estudos/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/estudos/screenmatch/model/DadosSerie.java
@@ -1,4 +1,10 @@
 package br.com.estudos.screenmatch.model;
 
-public record DadosSerie() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
--- src/main/java/br/com/estudos/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/estudos/screenmatch/service/ConverteDados.java
@@ -1,4 +1,18 @@
 package br.com.estudos.screenmatch.service;
 
-public class ConverteDados {
+import br.com.estudos.screenmatch.model.DadosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
